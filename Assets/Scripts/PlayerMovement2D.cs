40e0682ff22af0963c0403d97f0d2899192d3948
Shamitha Pichika <spichika@seattleu.edu>
2026-01-20 23:12:35 -0800

    add player movement script
    added functionality for the player to move

---
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    public float speed = 3f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
