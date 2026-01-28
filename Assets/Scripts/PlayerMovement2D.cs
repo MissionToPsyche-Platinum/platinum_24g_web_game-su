46877024bbb15dbe0a788c445d443b7b304cc60c
Shamitha Pichika <spichika@seattleu.edu>
2026-01-27 20:46:18 -0800

    updated playermovement2D.cs
    added conditionals for the new animated movement features

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -3,19 +3,54 @@ using UnityEngine;
 public class PlayerMovement2D : MonoBehaviour
 {
     public float speed = 3f;
+
     private Rigidbody2D rb;
+    private Animator anim;
+
     private Vector2 movement;
+    private Vector2 lastMoveDir = Vector2.down;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement = movement.normalized;
+        Vector2 raw = new Vector2(
+
+        Input.GetAxisRaw("Horizontal"),
+        Input.GetAxisRaw("Vertical")
+
+        );
+
+        if (Mathf.Abs(raw.x) > Mathf.Abs(raw.y))
+            raw.y = 0;
+        else
+            raw.x = 0;
+
+        bool isMoving = raw.sqrMagnitude > 0.01f;
+
+        if (isMoving)
+        {
+            movement = raw.normalized;
+            lastMoveDir = movement;
+        }
+        else
+        {
+            movement = Vector2.zero;
+        }
+
+        Vector2 animDir = lastMoveDir;
+        if (isMoving)
+            animDir = movement;  
+
+        anim.SetFloat("MoveX", animDir.x);
+        anim.SetFloat("MoveY", animDir.y);
+        anim.SetBool("IsMoving", isMoving);
+        anim.SetFloat("LastMoveX", lastMoveDir.x);
+        anim.SetFloat("LastMoveY", lastMoveDir.y);
     }
 
     void FixedUpdate()
