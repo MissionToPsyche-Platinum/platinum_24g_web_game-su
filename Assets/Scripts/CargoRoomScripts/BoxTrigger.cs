9f72bf883fb103aafbdffdd97ebcb1040a925ca7
abbyhayd <abigayle.doinog@gmail.com>
2026-04-08 13:14:56 -0700

    fixed how hints work in cargo rooms

--- Assets/Scripts/CargoRoomScripts/BoxTrigger.cs
+++ Assets/Scripts/CargoRoomScripts/BoxTrigger.cs
@@ -7,11 +7,9 @@ using UnityEngine.UI;
 
 public class BoxTrigger : MonoBehaviour
 {
-    private const string PopupName = "BoxPopUp";
-    private const string HintName = "BoxInteractHint";
 
     public GameObject popupPanel;
-    private GameObject hintLabel;
+    public GameObject hint;
     private GameObject startGameButton;
     private bool canInteract;
     private PlayerMovement2D playerMovement;
@@ -58,13 +56,10 @@ public class BoxTrigger : MonoBehaviour
         playerMovement = other.GetComponent<PlayerMovement2D>();
         playerRigidbody = other.GetComponent<Rigidbody2D>();
 
-        if (hintLabel == null)
+        if (hint == null)
         {
-            Canvas canvas = FindFirstObjectByType<Canvas>();
-            if (canvas != null)
-            {
-                EnsureHint(canvas);
-            }
+            Debug.LogError("BoxTrigger: hint is NOT assigned in Inspector.");
+            return; ;
         }
 
         ToggleHint(true);
@@ -128,9 +123,9 @@ public class BoxTrigger : MonoBehaviour
 
     private void ToggleHint(bool isVisible)
     {
-        if (hintLabel != null)
+        if (hint != null)
         {
-            hintLabel.SetActive(isVisible);
+            hint.SetActive(isVisible);
         }
     }
 
@@ -139,110 +134,4 @@ public class BoxTrigger : MonoBehaviour
         return popupPanel != null && popupPanel.activeSelf;
     }
 
-    /*
-    private void EnsurePopup()
-    {
-        if (popupPanel != null)
-        {
-            return;
-        }
-
-        Canvas canvas = FindFirstObjectByType<Canvas>();
-        if (canvas == null)
-        {
-            Debug.LogWarning("BoxPopup: No Canvas found in scene.");
-            return;
-        }
-
-        EnsureHint(canvas);
-
-        popupPanel = new GameObject(PopupName, typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
-        popupPanel.transform.SetParent(canvas.transform, false);
-
-        RectTransform panelTransform = popupPanel.GetComponent<RectTransform>();
-        panelTransform.anchorMin = new Vector2(0.5f, 0.5f);
-        panelTransform.anchorMax = new Vector2(0.5f, 0.5f);
-        panelTransform.pivot = new Vector2(0.5f, 0.5f);
-        panelTransform.sizeDelta = new Vector2(420f, 240f);
-        panelTransform.anchoredPosition = Vector2.zero;
-
-        Image panelImage = popupPanel.GetComponent<Image>();
-        panelImage.color = new Color(0.1f, 0.12f, 0.16f, 0.95f);
-
-
-        startGameButton = new GameObject("StartGameButton",
-            typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(Button));
-        startGameButton.transform.SetParent(popupPanel.transform, false);
-
-        RectTransform factButtonTransform = startGameButton.GetComponent<RectTransform>();
-        factButtonTransform.anchorMin = new Vector2(0.5f, 0.5f);
-        factButtonTransform.anchorMax = new Vector2(0.5f, 0.5f);
-        factButtonTransform.pivot = new Vector2(0.5f, 0.5f);
-        factButtonTransform.sizeDelta = new Vector2(220f, 60f);
-        factButtonTransform.anchoredPosition = new Vector2(0f, 45f);
-
-        Image factButtonImage = startGameButton.GetComponent<Image>();
-        factButtonImage.color = new Color(0.2f, 0.55f, 0.75f, 1f);
-
-        GameObject factLabelObject = new GameObject("Label", typeof(RectTransform), typeof(CanvasRenderer), typeof(Text));
-        factLabelObject.transform.SetParent(startGameButton.transform, false);
-
-        RectTransform factLabelTransform = factLabelObject.GetComponent<RectTransform>();
-        factLabelTransform.anchorMin = new Vector2(0f, 0f);
-        factLabelTransform.anchorMax = new Vector2(1f, 1f);
-        factLabelTransform.offsetMin = Vector2.zero;
-        factLabelTransform.offsetMax = Vector2.zero;
-
-        Text factLabel = factLabelObject.GetComponent<Text>();
-        factLabel.text = "Start minigame";
-        factLabel.alignment = TextAnchor.MiddleCenter;
-        factLabel.color = Color.white;
-        factLabel.fontSize = 24;
-        factLabel.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-
-        startGameButton.GetComponent<Button>().onClick.AddListener(() =>
-        {
-            if (Global.round <= MinigameSceneNames.Count)
-            {
-                SceneManager.LoadScene(MinigameSceneNames[Global.round - 1]);
-            }
-            else
-            {
-                int choice = Random.Range(0, MinigameSceneNames.Count);
-                SceneManager.LoadScene(MinigameSceneNames[choice]);
-            }
-        });
-
-        popupPanel.SetActive(false);
-    }
-    */
-
-    private void EnsureHint(Canvas canvas)
-    {
-        if (hintLabel != null)
-        {
-            return;
-        }
-
-        hintLabel = new GameObject(HintName, typeof(RectTransform), typeof(CanvasRenderer), typeof(Text));
-        hintLabel.transform.SetParent(canvas.transform, false);
-        hintLabel.transform.SetAsLastSibling();
-
-        RectTransform hintTransform = hintLabel.GetComponent<RectTransform>();
-        hintTransform.anchorMin = new Vector2(0.5f, 0.5f);
-        hintTransform.anchorMax = new Vector2(0.5f, 0.5f);
-        hintTransform.pivot = new Vector2(0.5f, 0.5f);
-        hintTransform.sizeDelta = new Vector2(300f, 40f);
-        hintTransform.anchoredPosition = new Vector2(0f, -140f);
-
-        Text hintText = hintLabel.GetComponent<Text>();
-        hintText.text = "Press E to interact";
-        hintText.alignment = TextAnchor.MiddleCenter;
-        hintText.color = Color.white;
-        hintText.fontSize = 18;
-        hintText.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-        hintText.raycastTarget = false;
-
-        hintLabel.SetActive(false);
-    }
 }
--- Assets/Scripts/CargoRoomScripts/RestarterScript.cs
+++ Assets/Scripts/CargoRoomScripts/RestarterScript.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 
 public class RestarterScript : MonoBehaviour
 {
-    private const string HintName = "BoxInteractHint";
 
     public GameObject helpPanel;
+    public GameObject hint;
     private GameObject player;
 
     private GameObject hintLabel;
@@ -38,13 +38,10 @@ public class RestarterScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (hintLabel == null)
+        if (hint == null)
         {
-            Canvas canvas = FindFirstObjectByType<Canvas>();
-            if (canvas != null)
-            {
-                EnsureHint(canvas);
-            }
+            Debug.LogError("RestarterScript: hint is NOT assigned in Inspector.");
+            return; ;
         }
 
         ToggleHint(true);
@@ -57,39 +54,10 @@ public class RestarterScript : MonoBehaviour
 
     private void ToggleHint(bool isVisible)
     {
-        if (hintLabel != null)
+        if (hint != null)
         {
-            hintLabel.SetActive(isVisible);
+            hint.SetActive(isVisible);
         }
-    }
-
-    private void EnsureHint(Canvas canvas)
-    {
-        if (hintLabel != null)
-        {
-            return;
-        }
-
-        hintLabel = new GameObject(HintName, typeof(RectTransform), typeof(CanvasRenderer), typeof(Text));
-        hintLabel.transform.SetParent(canvas.transform, false);
-        hintLabel.transform.SetAsLastSibling();
-
-        RectTransform hintTransform = hintLabel.GetComponent<RectTransform>();
-        hintTransform.anchorMin = new Vector2(0.5f, 0.5f);
-        hintTransform.anchorMax = new Vector2(0.5f, 0.5f);
-        hintTransform.pivot = new Vector2(0.5f, 0.5f);
-        hintTransform.sizeDelta = new Vector2(300f, 40f);
-        hintTransform.anchoredPosition = new Vector2(0f, -140f);
-
-        Text hintText = hintLabel.GetComponent<Text>();
-        hintText.text = "Press E to interact";
-        hintText.alignment = TextAnchor.MiddleCenter;
-        hintText.color = Color.white;
-        hintText.fontSize = 18;
-        hintText.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-        hintText.raycastTarget = false;
-
-        hintLabel.SetActive(false);
     }
 
     public void RestartMinigame()
