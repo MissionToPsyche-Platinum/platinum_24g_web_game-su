c39b6e8edb6b87a4d398fa4eef9ef7e2c3babd43
Shamitha Pichika <spichika@seattleu.edu>
2026-05-31 01:56:40 -0700

    added sound effect to torch in collision repair game

--- Assets/Scripts/RepairCollisionMinigameScripts/WeldGunScript.cs
+++ Assets/Scripts/RepairCollisionMinigameScripts/WeldGunScript.cs
@@ -5,13 +5,36 @@ public class WeldGunScript : MonoBehaviour
     public GameObject weldSpark;
     public GameObject helpPanel;
 
+    // torch audio
+    public AudioSource torchAudioSource;
+    public AudioClip torchLoopSound;
+
     // Update is called once per frame
     void Update()
     {
         FollowCursor();
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+
+        if (Input.GetMouseButtonDown(0))
         {
             TriggerSpark();
+
+            //play torch audio while using it
+            if (torchAudioSource != null && torchLoopSound != null)
+            {
+                torchAudioSource.clip = torchLoopSound;
+                torchAudioSource.loop = true;
+                torchAudioSource.Play();
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            TriggerSpark();
+
+            if (torchAudioSource != null)
+            {
+                torchAudioSource.Stop();
+            }
         }
     }
 
@@ -20,6 +43,7 @@ public class WeldGunScript : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         mousePosition.z = transform.position.z;
+
         transform.position = mousePosition - new Vector3(-1.5f, 1.5f);
     }
 
